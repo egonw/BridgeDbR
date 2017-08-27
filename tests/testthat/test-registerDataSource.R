581e5503c95996042d51a4753ad110310d60e4f3
Egon Willighagen <egon.willighagen@gmail.com>
2017-08-27 15:22:26 +0200

    Updated to BridgeDb 2.2.0 and more unit tests.

---
+++ tests/testthat/test-registerDataSource.R
@@ -0,0 +1,15 @@
+library(BridgeDbR)
+context("simple")
+
+test_that("allows registring of an existing resources", {
+    dataSource = registerDataSource(code="Cks", name="KNApSAcK")
+    expect_false(is.null(dataSource))
+    expect_equal(dataSource$getSystemCode(), "Cks")
+})
+
+test_that("allows registring of a new resources", {
+    dataSource = registerDataSource(code="Sou", name="Sourcy McSourceface")
+    expect_false(is.null(dataSource))  
+    expect_equal(dataSource$getSystemCode(), "Sou")
+})
+
