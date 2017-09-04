689c73bf629d71438822b0bba3494b99e68c01c5
Egon Willighagen <egon.willighagen@gmail.com>
2017-09-04 09:05:20 +0200

    Added two more unit tests (one failing)

---
+++ tests/testthat/test-getMatchingSources.R
@@ -0,0 +1,16 @@
+library(BridgeDbR)
+context("simple")
+
+test_that("it recognizes identifiers from HMDB (old)", {
+    answerList = getMatchingSources("HMDB00626")
+    expect_true(is.vector(answerList))
+    expect_true("HMDB" %in% answerList)
+})
+
+test_that("it recognizes identifiers from HMDB (new, 2017-08)", {
+    answerList = getMatchingSources("HMDB0000626")
+    expect_true(is.vector(answerList))
+    expect_true("HMDB" %in% answerList)
+})
+
+
