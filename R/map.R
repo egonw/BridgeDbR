f951a125dc2ce1e96472ef588c693882183b26ac
Egon Willighagen <egon.willighagen@gmail.com>
2020-04-19 19:34:31 +0200

    the map() method now returns a data frame

--- R/map.R
+++ R/map.R
@@ -12,7 +12,14 @@ map <- function(mapper, source, identifier, target=NULL){
   str <- strsplit(str[[1]][2],"\\]")
   str <- strsplit(str[[1]][1],", ")
   foo <- unlist(str)
-  bar <- sapply(foo, function(x) { unlist(substr(x,regexpr(":",x,fixed=TRUE)+1,nchar(x)))})
-  names(bar) <- c()
-  bar
+  targetcodes <- sapply(foo, function(x) { unlist(substr(x,0,regexpr(":",x,fixed=TRUE)-1))})
+  names(targetcodes) <- c()
+  mappings <- sapply(foo, function(x) { unlist(substr(x,regexpr(":",x,fixed=TRUE)+1,nchar(x)))})
+  names(mappings) <- c()
+  data.frame(
+   source = rep(source, length(mappings)),
+   identifier = rep(identifier, length(mappings)),
+   target = targetcodes,
+   mapping = mappings
+  )
 }
