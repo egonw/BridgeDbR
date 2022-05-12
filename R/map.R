0a492c083dd47f077c0c7530ba4503a51ca7c720
Egon Willighagen <egon.willighagen@gmail.com>
2022-05-12 13:46:04 +0200

    Extract the isPrimary info

--- R/map.R
+++ R/map.R
@@ -12,14 +12,16 @@ map <- function(mapper, source, identifier, target=NULL){
   str <- strsplit(str[[1]][2],"\\]")
   str <- strsplit(str[[1]][1],", ")
   foo <- unlist(str)
-  targetcodes <- sapply(foo, function(x) { unlist(substr(x,0,regexpr(":",x,fixed=TRUE)-1))})
+  targetcodes <- sapply(foo, function(x) { unlist(strsplit(x,":")[[1]])[1] })
   names(targetcodes) <- c()
-  mappings <- sapply(foo, function(x) { unlist(substr(x,regexpr(":",x,fixed=TRUE)+1,nchar(x)))})
+  mappings <- sapply(foo, function(x) { unlist(strsplit(x,":")[[1]])[2] })
   names(mappings) <- c()
+  isPrimary <- sapply(foo, function(x) { unlist(strsplit(x,":")[[1]])[3] })
   data.frame(
    source = rep(source, length(mappings)),
    identifier = rep(identifier, length(mappings)),
    target = targetcodes,
-   mapping = mappings
+   mapping = mappings,
+   isPrimary = isPrimary
   )
 }
