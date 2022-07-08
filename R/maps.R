76c1107e0fc0bfceffe09bdd3eedb47fdfe67e7f
Tooba Abbassi-Daloii <31986271+tabbassidaloii@users.noreply.github.com>
2022-07-08 13:08:43 +0200

    Update maps.R
    together with @ozancinar, I suggest updating the maps function

--- R/maps.R
+++ R/maps.R
@@ -1,25 +1,16 @@
-maps <- function(mapper, identifiers, target=NULL){
+maps <- function(mapper, identifiers, target = NULL){
   if (!is.data.frame(identifiers)) stop("The provided identifiers parameter must be a data frame.")
-  allResults <- data.frame(
-    source = character(),
-    identifier = character(),
-    target = character(),
-    mapping = character()
-  )
-  for (i in 1:nrow(identifiers)) {
-    source = as.character(identifiers$source[[i]])
-    identifier = as.character(identifiers$identifier[[i]])
+  allResults <- do.call(rbind, suppressWarnings (apply (identifiers, 1, function (x) {
+    source = x["source"]  
+    identifier = x["identifier"]
     if (is.null(target)) {
-      singleResults <- map(
+      map(
         mapper, source=source, identifier=identifier
       )
     } else {
-      singleResults <- map(
+      map(
         mapper, source=source, identifier=identifier,
-        target=target
-      )
-    }
-    allResults <- rbind(allResults, singleResults)
-  }
-  allResults
+        target = target
+      )}
+  })))
 }
