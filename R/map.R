51090ca49b3f86087fd2f40022f867e42f51a61e
Tooba Abbassi-Daloii <31986271+tabbassidaloii@users.noreply.github.com>
2022-07-08 12:21:20 +0200

    Update map.R
    The compactIdentifier = NA can be removed unless we add it to the R Documentation

--- R/map.R
+++ R/map.R
@@ -1,33 +1,29 @@
-map <- function(mapper, source, identifier, compactIdentifier=NA, target=NULL){
-  if (!is.na(compactIdentifier)) {
-    source <- gsub(":.*", "", compactIdentifier)
-    identifier <- gsub(".*:", "", compactIdentifier)
-    datasource <- getDataSource(prefix=source)
-  } else {
+map <- function(mapper, source, identifier, compactIdentifier = NA, target = NULL){
+  # if (!is.na(compactIdentifier)) { # this can be removed unless we add it to the R Documentation
+
+  #   source <- gsub(":.*", "", compactIdentifier)
+  #   identifier <- gsub(".*:", "", compactIdentifier)
+  #   datasource <- getDataSource(prefix=source)
+  # } else {
     datasource <- getDataSource(code=source)
-  }
+  # }
   xref <- .jnew("org/bridgedb/Xref", identifier, datasource)
   if (!is.null(target)) {
-    datatarget <- getDataSource(code=target)
-    targets <- .jarray(c(datatarget), contents.class="org/bridgedb/DataSource")
+    datatarget <- getDataSource(code = target)
+    targets <- .jarray(c(datatarget), contents.class ="org/bridgedb/DataSource")
   } else {
-    targets <- .jarray(list(), contents.class="org/bridgedb/DataSource")
+    targets <- .jarray(list(), contents.class = "org/bridgedb/DataSource")
   }
   mappings = mapper$mapID(xref, targets)
-  str <- strsplit(mappings$toString(),"\\[")
-  str <- strsplit(str[[1]][2],"\\]")
-  str <- strsplit(str[[1]][1],", ")
-  foo <- unlist(str)
-  targetcodes <- sapply(foo, function(x) {gsub(":.*","",x)})
-  names(targetcodes) <- c()
-  mappings <- sapply(foo, function(x) {gsub(":[^:]*$|^[^:]*:","",x)})
-  names(mappings) <- c()
-  isPrimary <- sapply(foo, function(x) {gsub(".*:","",x)})
+  str <- strsplit(gsub(".*\\[|].*", "", mappings$toString()), ", ")
+  targetcodes <- lapply(str, function(x) {gsub(":.*", "", x, )})
+  mappings <- lapply(str, function(x) {gsub(":[^:]*$|^[^:]*:", "", x)})
+  isPrimary <- lapply(str, function(x) {gsub(".*:", "", x)})
   data.frame(
-   source = rep(source, length(mappings)),
-   identifier = rep(identifier, length(mappings)),
-   target = targetcodes,
-   mapping = mappings,
-   isPrimary = isPrimary
+    source = source,
+    identifier = identifier,
+    target = unlist(targetcodes, use.names = FALSE),
+    mapping = unlist(mappings, use.names = FALSE),
+    isPrimary = unlist(isPrimary, use.names = FALSE)
   )
 }
