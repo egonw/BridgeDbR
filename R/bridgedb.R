fdeb1ed0244ff71cb28091c74b346ac7bd88849f
Egon Willighagen <egon.willighagen@gmail.com>
2023-02-10 08:39:57 +0100

    Upgraded to BridgeDb 3.0.19 and Derby 10.15.2

--- R/bridgedb.R
+++ R/bridgedb.R
@@ -12,8 +12,11 @@ require(rJava, quietly=TRUE)
         "org.bridgedb.jar",
         "org.bridgedb.bio.jar",
         "org.bridgedb.rdb.jar",
-        "derby.jar"
-    	)
+        "derby-10.15.2.0.jar",
+        "derbyclient-10.15.2.0.jar",
+        "derbyshared-10.15.2.0.jar",
+        "derbytools-10.15.2.0.jar"
+    )
     .jpackage(pkg, jars=libJars)
 #make sure the biodatasources are instantiated
 .jcall("org/bridgedb/bio/DataSourceTxt","V","init")
