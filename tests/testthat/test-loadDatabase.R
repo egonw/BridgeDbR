9d63081799b020d5edacc6481ad55a0b060116a7
Egon Willighagen <egon.willighagen@gmail.com>
2023-09-09 15:56:48 +0200

    Loaded database is not NULL

--- tests/testthat/test-loadDatabase.R
+++ tests/testthat/test-loadDatabase.R
@@ -4,4 +4,5 @@ context("simple")
 test_that("it can load Derby files", {
     location = paste(getwd(), "test.bridge", sep = "/")
     db <- loadDatabase(location)
+    expect_false(is.null(db))
 })
